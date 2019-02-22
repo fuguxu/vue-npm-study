44b6a1308fb5b2e6fea5e1c2b53e2877ff0e3e2e
fuguxu <guangxue.fu@partner.midea.com>
2019-02-22 16:32:43 +0800

    插件修改

--- src/main.js
+++ src/main.js
@@ -1,8 +1,8 @@
 import Vue from 'vue'
 import App from './App.vue'
 import plugin from './plugin/index';
-console.log(plugin)
 Vue.use(plugin);//注册插件
+
 new Vue({
   el: '#app',
   render: h => h(App)
